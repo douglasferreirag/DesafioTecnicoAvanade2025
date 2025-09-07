85c49c9ef67c91fd1c17bc662f37b48fc881f6ed
douglasferreirag <douglas.ferreirag@gmail.com>
2025-09-06 23:26:44 -0300

    Files Program.cs createad

--- Services/SalesService/SalesService/obj/Debug/net9.0/SalesService.AssemblyInfo.cs
+++ Services/SalesService/SalesService/obj/Debug/net9.0/SalesService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c020d92e699d04855bc45c64c32c56e12cb071df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5556b58b68e79e205d3532126a34f32553d04398")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
